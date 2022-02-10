9fde1ace301ac0d402f38753de694891b0689c03
NooriAbdullah <abdullahn_1997@yahoo.com>
2022-02-10 11:07:31 -0800

    3-1

--- assignment3-1.cpp
+++ assignment3-1.cpp
@@ -1,6 +1,26 @@
 #include <iostream> 
 using namespace std;
+
 int main ()
 {
+ int classA;
+ int classB;
+ int classC;
+
+int priceA = 15;
+int priceB =12;
+int priceC =9; 
+
+int total;
+
+cout << "enter the number of tickets sold for class A:";
+cin >> classA;
+cout <<"enter the number of tickets sold for class B:";
+cin >> classB;
+cout <<"enter the number of tickets sold for class C:";
+cin >> classC;
+
+total = (classA* priceA) + (classB * priceB) + (classC * priceC);
+cout <<"total price fro ticket sold"<< total << endl;
 
 }
