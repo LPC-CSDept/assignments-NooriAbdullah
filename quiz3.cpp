c0b225fadac439d9586b25e945aa5885a678f892
NooriAbdullah <abdullahn_1997@yahoo.com>
2022-02-22 11:30:42 -0800

    quiz 3

---
+++ quiz3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+int main ()
+{
+int num1, num2, num3;
+srand()
+
+num1 rand() %100;
+cout<< num1<<endl;
+
+num2 rand()%100;
+cout<<num1<<endl;
+
+num3 rand()%100;
+cout<<num1<<endl;
+
+sum=num1+num2=num3;
+avg=sum/3.0 
+}
